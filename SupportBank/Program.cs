b3e519a0661423a0ff8d986d091f48ca624259ab
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-15 13:35:02 +0100

    Refactored out logging

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -11,19 +11,7 @@ namespace SupportBank
 
         private static void Main(string[] args)
         {
-            // Set up logging
-            var config = new LoggingConfiguration();
-
-            var target = new FileTarget { FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
-            config.AddTarget("File Logger", target);
-            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
-
-            var consoleTarget = new ConsoleTarget {Layout = @"${level}: ${message}"};
-            config.AddTarget("Console", consoleTarget);
-            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Error, consoleTarget));
-
-            LogManager.Configuration = config;
-            var logger = NLog.LogManager.GetCurrentClassLogger();
+            StartLogging();
 
             var people = new Dictionary<string, Person>();
 
@@ -35,6 +23,21 @@ namespace SupportBank
             cliInterface.RunUserCommandLoop();
 
             Logger.Debug("Finishing normally...");
+        }
+
+        private static void StartLogging()
+        {
+            var config = new LoggingConfiguration();
+
+            var target = new FileTarget { FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+            config.AddTarget("File Logger", target);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+
+            var consoleTarget = new ConsoleTarget { Layout = @"${level}: ${message}" };
+            config.AddTarget("Console", consoleTarget);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Error, consoleTarget));
+
+            LogManager.Configuration = config;
         }
     }
 }
