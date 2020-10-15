dc2b23456faeeb252cf3972283ed4c178f0799d6
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-15 16:13:54 +0100

    Added file input option for console interface

--- SupportBank/CliInterface.cs
+++ SupportBank/CliInterface.cs
@@ -9,6 +9,8 @@ namespace SupportBank
         public static void RunUserCommandLoop(Dictionary<string, Person> people)
         {
             var listAccountMatcher = new Regex("List (.*)");
+            var inputFileMatcher = new Regex(@"Input File (.*)");
+
             while (true)
             {
                 Program.Logger.Debug("Presenting main menu");
@@ -16,9 +18,14 @@ namespace SupportBank
                 Console.WriteLine("\n\"List All\" to list all balances");
                 Console.WriteLine("\"Quit\" to quit");
                 Console.WriteLine("\"List [Account]\"");
+                Console.WriteLine("\"Input File [filename]\"");
                 Console.WriteLine("Please type an option: ");
 
                 var input = Console.ReadLine();
+
+                var listAccountMatch = listAccountMatcher.Match(input);
+                var inputFileMatch = inputFileMatcher.Match(input);
+
 
                 if (input == "Quit")
                 {
@@ -32,11 +39,10 @@ namespace SupportBank
                     Console.WriteLine("\n");
                     PrintBalances(people);
                 }
-                else if (listAccountMatcher.IsMatch(input))
+                else if (listAccountMatch.Success)
                 {
                     Program.Logger.Debug("List [Account]");
-                    var match = listAccountMatcher.Match(input);
-                    var name = match.Groups[1].ToString();
+                    var name = listAccountMatch.Groups[1].ToString();
 
                     if (people.ContainsKey(name))
                     {
@@ -49,6 +55,11 @@ namespace SupportBank
                         Console.WriteLine($"Person: {name} wasn't found");
                     }
                 }
+                else if (inputFileMatch.Success)
+                {
+                    Program.Logger.Debug("Input File [filename]");
+                    ReadFromFile(inputFileMatch.Groups[1].ToString(), people);
+                }
                 else
                 {
                     Program.Logger.Debug($"Unrecognised input: {input}");
@@ -60,6 +71,35 @@ namespace SupportBank
         private static void PrintBalances(Dictionary<string, Person> people)
         {
             foreach (var person in people.Values) Console.WriteLine(person.BalanceStatus());
+        }
+
+        private static void ReadFromFile(string filename, Dictionary<string, Person> people)
+        {
+            var fileTypeMatcher = new Regex(@".*\.((csv)|(json))");
+
+            if (fileTypeMatcher.IsMatch(filename))
+            {
+                if (fileTypeMatcher.Match(filename).Groups[1].ToString() == "csv")
+                {
+                    Program.Logger.Debug($"Working on CSV: {filename}");
+                    CsvReader.ReadCsv(filename, people);
+                }
+                else if (fileTypeMatcher.Match(filename).Groups[1].ToString() == "json")
+                {
+                    Program.Logger.Debug($"Working on JSON: {filename}");
+                    JsonReader.ReadJson(filename, people);
+                }
+                else
+                {
+                    Program.Logger.Fatal("Major error parsing file name, matched with file extension "
+                                         + fileTypeMatcher.Match(filename).Groups[1].ToString());
+                }
+            }
+            else
+            {
+                Program.Logger.Debug($"Unsupported file type for file: {filename}");
+                Console.WriteLine("Unsupported file type.");
+            }
         }
     }
 }
