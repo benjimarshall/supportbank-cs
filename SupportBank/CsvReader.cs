389ee7b2ab7b0847604cc366551806c0ac569989
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-15 10:35:32 +0100

    Added error handling for dodgy CSVs

--- SupportBank/CsvReader.cs
+++ SupportBank/CsvReader.cs
@@ -33,12 +33,42 @@ namespace SupportBank
             // Skip first line
             if (fields[0] == "Date") return;
 
+            if (fields.Length < 5)
+            {
+                Program.Logger.Error($"Not enough fields for transaction on {parser.LineNumber} of {filename}");
+                return;
+            }
+
             // Fields are: 0: Date, 1: From, 2: To, 3: Narrative, 4: Amount
-            var date = DateTime.Parse(fields[0]);
+            DateTime date;
+            try
+            {
+                date = DateTime.Parse(fields[0]);
+            }
+            catch (FormatException e)
+            {
+                Program.Logger.Error(
+                    $"Could not parse date \"{fields[0]}\" for transaction on line {parser.LineNumber} in {filename}"
+                );
+                return;
+            }
+
             var fromPerson = FindOrAddPerson(fields[1], people);
             var toPerson = FindOrAddPerson(fields[2], people);
             var narrative = fields[3];
-            var amount = double.Parse(fields[4]);
+
+            double amount;
+            try
+            {
+                amount = double.Parse(fields[4]);
+            }
+            catch (FormatException e)
+            {
+                Program.Logger.Error(
+                    $"Could not parse amount \"{fields[4]}\" for transaction on line {parser.LineNumber} in {filename}"
+                );
+                return;
+            }
 
             var transaction = new Transaction(date, fromPerson, toPerson, narrative, amount);
 
